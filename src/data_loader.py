49d1c3016092fb808f5d634c22e9da8e340d7af2
MiguelAngelPerdomo <160289535+MiguelAngelPerdomo@users.noreply.github.com>
2025-04-19 10:21:48 -0500

    Update data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import tensorflow as tf
+
+url = "https://raw.githubusercontent.com/dD2405/Twitter_Sentiment_Analysis/master/train.csv"
+csv_path = tf.keras.utils.get_file("twitter_sentiment.csv", url)
+df = pd.read_csv(csv_path)
+
+df = df[["tweet", "label"]]
